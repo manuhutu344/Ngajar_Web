47d5aab76536f795d52ed73a3c2081538765527a
Manuhutu344 <nandoperdos2@gmail.com>
2024-09-26 18:52:29 +0900

    Untuk Ngajar Web

---
+++ coba_saja/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Nav from './Navigations/Nav'
+import Products from './Products/Products'
+import Recommended from "../src/Recommended/Recommended"
+
+function App() {
+
+  return (
+    <>
+    <Nav />
+    <Products />
+    <Recommended />
+    </>
+  )
+}
+
+export default App
---
+++ coba_saja/src/Recommended/Recommended.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Recommended.css"
+
+function Recommended() {
+  return (
+    <div>Recommended</div>
+  )
+}
+
+export default Recommended
