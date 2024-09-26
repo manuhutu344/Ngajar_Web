ec2f859726cf8e83581f8dd8f4874d137d2e9809
Manuhutu344 <nandoperdos2@gmail.com>
2024-09-26 19:23:51 +0900

    Sampai di membuat Produtcs

--- coba_saja/src/Navigations/Nav.jsx
+++ coba_saja/src/Navigations/Nav.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Nav.css"
+import {FiHeart} from "react-icons/fi"
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from "react-icons/ai"
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="nav-container">
+          <input type='text' placeholder="Carii" className="search-input" />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
   )
 }
 
--- coba_saja/src/Products/Products.jsx
+++ coba_saja/src/Products/Products.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import "./Products.css"
+import { AiFillStar } from 'react-icons/ai'
+import {BsFillBagHeartFill} from "react-icons/bs"
 
 function Products() {
   return (
-    <div>Products</div>
+    <>
+    <section className="card-container">
+      <section className="card">
+        <img src="httpBsFillBagHeartFills://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Sepatu" />
+        <div className="card-details">
+          <h3 className="card-title">
+            Sepatu
+          </h3>
+          <section className="card-reviews">
+            <AiFillStar className="rating-star" />  <AiFillStar className="rating-star" />
+            <span className="total-reviews">
+              4
+            </span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>
+                Rp.20.000
+              </del>
+              Rp.1000
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </section>
+    </>
   )
 }
 
